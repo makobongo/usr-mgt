d7635ffbb73501e27f0cbef0f1f05b608b59eb0e
Steve Obongo <steveobongo0@gmail.com>
2025-11-22 15:32:27 +0300

    refactor: adjusted to include user input fields to allow user registraion

--- frontend/src/components/auth/Register.vue
+++ frontend/src/components/auth/Register.vue
@@ -1,5 +1,31 @@
 <template>
-    register
+  <v-container fluid>
+    <v-row>
+      <!-- Login Section -->
+      <v-col
+        cols="12"
+        md="6"
+        class="d-flex justify-center align-center"
+      >
+        <v-card class="pa-5" width="400">
+          <v-card-title class="text-center">Register</v-card-title>
+          <v-form>
+            <v-text-field label="Name"  variant="outlined" density="compact" required />
+            <v-text-field label="Email"  variant="outlined" density="compact" required />
+            <v-text-field label="Phone"  variant="outlined" density="compact" required />
+            <v-text-field label="Password" type="password" variant="outlined" density="compact" required />
+            <v-btn color="primary" block variant="flat">Login</v-btn>
+            <router-link :to="{ name: 'login'}">login</router-link>
+          </v-form>
+        </v-card>
+      </v-col>
+
+      <!-- Carousel Section (visible only on large screens) -->
+      <Carousel />
+    </v-row>
+  </v-container>
 </template>
-<script>
-export default{}</script>
+
+<script setup>
+import { Carousel } from '../includes';
+</script>
