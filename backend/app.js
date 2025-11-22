cad4d03f5a7b9207f71fd36f2e5f692d5efb4c86
Steve Obongo <steveobongo0@gmail.com>
2025-11-22 20:53:22 +0300

    update: configured body-parser

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,14 @@ const app = express();
 const port = 3001;
 var loginRouter = require('./routes/auth/login');
 var registerRouter = require('./routes/auth/register');
+var bodyParser = require('body-parser');
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
 
 app.use('/api/auth', loginRouter);
 app.use('/api/auth', registerRouter);
+
 
 // Start the server
 app.listen(port, () => {
