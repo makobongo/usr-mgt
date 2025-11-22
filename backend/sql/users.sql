c265f2dd8eea02177e4857a49e893c87525caf7f
Steve Obongo <steveobongo0@gmail.com>
2025-11-22 23:08:16 +0300

    fix: added unique contraint to email and phone number field

--- backend/sql/users.sql
+++ backend/sql/users.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id CHAR(36) PRIMARY KEY,
     name varchar(255),
-    email varchar(255),
-    phone varchar(255),
+    email varchar(255) UNIQUE,
+    phone varchar(255) UNIQUE,
     password varchar(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
