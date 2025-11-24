c09cb7671c0c37723ed03feea01be32b2467b8ed
Steve Obongo <steveobongo0@gmail.com>
2025-11-24 22:12:45 +0300

    feature: created controller for login

---
+++ backend/controllers/auth/login.js
@@ -0,0 +1,38 @@
+var bcrypt = require('bcryptjs');
+var connection = require('../../config/db');
+const { validationResult } = require('express-validator');
+
+exports.login = async (req, res, next) => {
+  try {
+    //handle validation of the input fields
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // If validation passes, proceed with business logic
+    const { email, password } = req.body;
+    connection.execute('SELECT email, password FROM users WHERE email = ?', [email], async(error, results, fields) => {
+      if(results.length){
+        // Compare plaintext vs hashed
+        const isMatch = await bcrypt.compare(password, results[0]['password']);
+        if(!isMatch){
+          return res.json({
+            message: 'Sorry! wrong credentials'
+          })
+        } else {
+          return res.json({
+            message: 'Logged in successfully'
+          })
+        }
+      }else {
+        return res.json({
+          message: 'Sorry! wrong credentials'
+        })
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Sorry! something went wrong'
+    })
+  }
+}
