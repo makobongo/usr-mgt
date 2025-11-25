12721e5853506b962ca935f5b6a9fb9af1288b5e
Steve Obongo <steveobongo0@gmail.com>
2025-11-25 15:39:11 +0300

    update: used environment set jwt expiration time

--- backend/controllers/auth/login.js
+++ backend/controllers/auth/login.js
@@ -21,7 +21,7 @@ exports.login = async (req, res, next) => {
             message: 'Sorry! wrong credentials'
           })
         } else {
-          const token = jwt.sign({ email: results[0]['email'] },process.env.JWT_SECRET,{ expiresIn: '5m' })
+          const token = jwt.sign({ email: results[0]['email'] },process.env.JWT_SECRET,{ expiresIn: process.env.JWT_EXPIRATION })
           return res.json({
             message: token
           })
