667f6640e74b70ae1e4e54fb0431f1877ce00698
Steve Obongo <steveobongo0@gmail.com>
2025-12-07 14:50:49 +0300

    update: added validation of the input fields

--- frontend/src/components/auth/UserLogin.vue
+++ frontend/src/components/auth/UserLogin.vue
@@ -17,14 +17,14 @@
             />
             <v-text-field
               label="Password"
-              :type=" showPassword ? 'text' : 'password' "
+              :type="showPassword ? 'text' : 'password'"
               v-model="form.password"
               variant="outlined"
               density="compact"
               required
               prepend-inner-icon="mdi-lock"
-              :append-inner-icon=" showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append-inner=" showPassword = !showPassword "
+              :append-inner-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append-inner="showPassword = !showPassword"
             />
             <v-btn color="primary" block variant="flat" @click="loginUser"
               >Login</v-btn
@@ -47,30 +47,38 @@ import { createUser } from "@/api/auth/login";
 import { inject } from "vue";
 const swal = inject("$swal");
 
-
 const showPassword = ref(false);
 const form = ref({
   email: "",
   password: "",
 });
 const loginUser = async () => {
-  try {
-    const res = await createUser(form.value);
-    console.log("message:", res.data);
-  } catch (error) {
-    console.log(error.response.data.message);
-    if (error.status == 404) {
-      swal.fire({
-        title: "Sorry!",
-        text: error.response.data.message,
-        icon: "error",
-      });
-    } else {
-      swal.fire({
-        title: "Error!",
-        text: "Something went wrong!",
-        icon: "error",
-      });
+  if (form.value.email == "" || form.value.password == "") {
+    swal.fire({
+      title: "NO CREDENTIALS PROVIDED!",
+      text: "Kindly provide your credentials",
+      icon: "info",
+    });
+  } else {
+    try {
+      const res = await createUser(form.value);
+      console.log("message:", res.data);
+    } catch (error) {
+      console.log(error.response.data.message);
+      if (error.status == 404) {
+        swal.fire({
+          title: "Sorry!",
+          text: error.response.data.message,
+          icon: "error",
+        });
+      } else {
+        swal.fire({
+          title: "Error!",
+          text: "Something went wrong!",
+          icon: "error",
+          width: "400px",
+        });
+      }
     }
   }
 };
