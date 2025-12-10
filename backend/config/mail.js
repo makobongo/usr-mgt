f344eae87fb0cf5fcfe06e871fcba90a8e44827a
Steve Obongo <steveobongo0@gmail.com>
2025-12-10 12:20:40 +0300

    feature: added config for nodemailer package

---
+++ backend/config/mail.js
@@ -0,0 +1,15 @@
+const nodemailer = require("nodemailer");
+
+// Create a test account or replace with real credentials.
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  },
+});
+
+
+module.exports = transporter;
