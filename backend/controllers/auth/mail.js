d0c87bb15f06e941d481b4646da97354fc8531ef
Steve Obongo <steveobongo0@gmail.com>
2025-12-10 14:13:43 +0300

    update: adjusted the message send to user via email

--- backend/controllers/auth/mail.js
+++ backend/controllers/auth/mail.js
@@ -7,8 +7,7 @@ exports.sendEmail = async (req, res) => {
         from: process.env.MAIL_FROM_ADDRESS,
         to: "lokomoscou@strelizia.site",
         subject: "Your OTP Code",
-        text: `Hi, We appreciate your partnership! Use OTP ${verificationNumber} to continue with your secure transaction. This OTP is valid for 10 minutes and can only be used once. For your security, please do not share this code with anyone.`,
-        html: "<a href='sbong.xyz' target='_blank'>USER MANAGEMENT</a>", // HTML body
+        html: `<p style='font-size:14px;'>Hi, <br />We appreciate your partnership! Use OTP <b style='color:black;font-weight:bold;'>${verificationNumber}</b> to continue with your secure transaction. This OTP is valid for <b style='color:black;font-weight:bold;'>10 minutes</b> and can only be used once. For your security, please do not share this code with anyone.</p>`,
     });
     return res.json({
         message: info.messageId
